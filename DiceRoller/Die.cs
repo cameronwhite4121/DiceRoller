fd94812ef664b7e4dfa6b30cbbd13bdb3989fa32
Cameron White <cameronwhite4121@gmail.com>
2024-07-12 12:51:51 -0700

    Add project files.

---
+++ DiceRoller/Die.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DiceRoller
+{
+	/// <summary>
+	/// Represents a single six sided die (1 - 6)
+	/// </summary>
+	public class Die
+	{
+		/// <summary>
+		/// The current value of the die that was rolled
+		/// </summary>
+        public byte FaceValue { get; set; }
+
+		/// <summary>
+		/// True if the die is held (the player wants to keep the value)
+		/// </summary>
+		public bool IsHeld { get; set; }
+
+		/// <summary>
+		/// Rolls the die and the roll is set to face value <see cref="FaceValue"/>
+		/// </summary>
+		/// <returns></returns>
+        public byte RollDie()
+		{
+			Random random = new Random();
+			FaceValue =  Convert.ToByte(random.Next(1, 7));
+			return FaceValue;
+		}
+	}
+}
---
+++ DiceRoller/Program.cs
@@ -0,0 +1,18 @@
+﻿using DiceRoller;
+
+// Construct die with number of sides
+Console.WriteLine("This program simply simulates rolling a die.");
+Die die1 = new Die();
+
+// Simulate rolling the die
+bool keepRolling = true;
+while (keepRolling == true) {
+	Console.Write("Your die rolled a ");
+	Console.WriteLine(die1.RollDie());
+	Console.Write("Keep rolling? (y/n) ");
+	string response = Console.ReadLine();
+	if(response.Equals("n"))
+	{
+		keepRolling = false; 
+	}
+}
